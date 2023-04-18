865031c8598135001a0558e0cdb0aae1066352e4
Ashley Collinge <me@ashleycollinge.co.uk>
2023-04-18 19:26:04 +0000

    Database connection working through docker
    Added check for db existing, if not create
    Fixed table creation issue
    Added sqlalchemy_utils pip dependency

--- Audiophile/run.py
+++ Audiophile/run.py
@@ -4,6 +4,7 @@
 from app.api.models import db, User
 from app import create_app
 from config import base
+from sqlalchemy_utils import database_exists, create_database
 
 
 app = create_app(base, db)
@@ -11,7 +12,10 @@ app = create_app(base, db)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        print(base.SQLALCHEMY_DATABASE_URI)
+        if not database_exists(base.SQLALCHEMY_DATABASE_URI):
+            create_database(base.SQLALCHEMY_DATABASE_URI)
+            db.create_all()
         # creates user if one doesn't exist
         #if User.query.filter_by(name='user') is None:
         #    usr = User(name='user', description='')
